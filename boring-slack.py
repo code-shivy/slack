5b0f0db92618412302a0c8cbcea2eea8a17ef29e
code-shivy <58331640+code-shivy@users.noreply.github.com>
2023-09-28 00:22:17 +0530

    Update boring-slack.py

--- boring-slack.py
+++ boring-slack.py
@@ -3,7 +3,7 @@ import json
 
 def send_slack_alert(message):
     post = {	"text":"{0}".format(message)}
-    slack_hook = "https://hooks.slack.com/services/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"
+    slack_hook = "Your slack hook"
     response_slack = requests.post(slack_hook, data=json.dumps(post),headers={'Content-Type': 'application/json'})
 
 
